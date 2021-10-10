5e757b3609965571bf54402e687057caf5b1155d
Daniela Maccali <daniela.maccali@gmail.com>
2021-10-10 18:24:00 -0300

    Estudo de implementação de classes

---
+++ ContaBancaria.cs
@@ -0,0 +1,55 @@
+﻿using System.Globalization;
+
+namespace cap5
+{
+    class ContaBancaria
+    {
+        private int _numeroConta;
+        private string nomeTitular;
+        private double saldoConta;
+        public double deposito;
+        public double saque;
+        public static double taxa = 5.00;
+
+        //Construtor da classe
+        public ContaBancaria(int numeroConta, string nomeTitular, double saldoConta)
+        {
+            _numeroConta = numeroConta;
+            this.nomeTitular = nomeTitular;
+            this.saldoConta = saldoConta;
+        }
+
+        public int NumeroConta
+        {
+            get { return _numeroConta; }
+            set { _numeroConta = value; }
+        }
+
+        public string NomeTitular
+        {
+            get { return nomeTitular; }
+            set { nomeTitular = value; }
+        }
+
+        public double SaldoConta
+        {
+            get { return saldoConta; }
+            set { saldoConta = value; }
+        }
+
+        public double AdicionarSaldo()
+        {
+            return saldoConta = saldoConta + deposito;
+        }
+
+        public double RemoverSaldo()
+        {
+            return saldoConta = saldoConta - saque - taxa;
+        }
+
+        public override string ToString()
+        {
+            return $"\tNúmero da Conta: {_numeroConta}\n\tNome do titular: {nomeTitular}\n\tsaldo da conta: R$ {saldoConta.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+
+namespace cap5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Número da conta: ");
+            int nConta = int.Parse(Console.ReadLine());
+            Console.Write("Nome do titular: ");
+            string nTitular = Console.ReadLine();
+            Console.Write("Depósito inicial? s/n : ");
+            double sConta = 0;
+            string resposta = Console.ReadLine();
+            if (resposta.ToUpper() == "S")
+            {
+                Console.Write("Digite o valor: R$ ");
+                sConta = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            }
+            else if (resposta.ToUpper() == "N")
+                {
+                    sConta = 0.00;
+                }
+            else
+            {
+                Console.WriteLine("Opção inválida! Digite S ou N : ");
+            }
+
+
+            //double depInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Instanciando uma classe (criando um novo objeto do tipo conta bancária) - lembrete: é posicional
+            ContaBancaria conta = new ContaBancaria(nConta, nTitular, sConta);
+
+            //Utiliza o método ToString na classe ContaBancaria
+            Console.WriteLine(conta);
+
+            Console.Write("Valor novo depósito: R$ ");
+            conta.deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.AdicionarSaldo();
+
+            Console.WriteLine(conta);
+
+            Console.Write("Valor do saque: R$ ");
+            conta.saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.RemoverSaldo();
+
+            Console.WriteLine(conta);
+
+            Console.ReadKey();
+        }
+    }
+}
